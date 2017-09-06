43e93e7f2f746ec2263e91695348ef9d294eb7fc
root <root@ip-172-31-23-0.ec2.internal>
2017-09-06 05:35:19 +0000

    testing

--- test.sh
+++ test.sh
@@ -1,1 +1,7 @@
-yum install http* -y
+#!bin/bash
+sudo -i
+yum install httpd -y
+yum update -y
+service httpd start
+chkconfig httpd on
+echo " <html><h1>Hello AWS Instance </h1><html> " > /var/www/html/index.html
