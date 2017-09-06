b2d25f4c7b6b1fb39d7ee6cf32c67d82e947643d
root <root@ip-172-31-23-0.ec2.internal>
2017-09-06 05:40:01 +0000

    testing

--- test.sh
+++ test.sh
@@ -1,7 +1,1 @@
-#!bin/bash
-sudo -i
-yum install httpd -y
-yum update -y
-service httpd start
-chkconfig httpd on
-echo " <html><h1>Hello AWS Instance </h1><html> " > /var/www/html/index.html
+touch file999999
